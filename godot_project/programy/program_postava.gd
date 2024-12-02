5ef3c6e64ada84f40cdc1c5bd803a40cb136ebb9
Macarat2009 <mat.becvar@gmail.com>
2024-12-02 17:03:05 +0100

    zmeny ammo

--- godot_project/programy/program_postava.gd
+++ godot_project/programy/program_postava.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
+
+@export var tricet := 30
+
 @export var speed := 200.0
+@export var ammo := tricet
+@export var reloading := false
+@export var strelba := false
 
 var projectile_scene := preload("res://projectile.tscn")
+
+func _ready() -> void:
+	show_ammo()
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -17,9 +26,23 @@ func _physics_process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	if Input.is_action_just_pressed("mouse_left"):
+	if Input.is_action_just_pressed("mouse_left") && ammo > 0 && reloading == false && strelba == false:
+		strelba = true
 		_shoot()
 		$AudioStreamPlayer2.play()
+		ammo = ammo - 1
+		show_ammo()
+		await get_tree().create_timer(0.2).timeout
+		strelba = false
+	if Input.is_action_just_pressed("R_key") && ammo < tricet:
+		reloading = true
+		ammo = 0
+		show_ammo()
+		$AudioStreamPlayer3.play()
+		await get_tree().create_timer(3.25).timeout
+		ammo = tricet
+		show_ammo()
+		reloading = false
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -31,6 +54,8 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
+func show_ammo():
+	%AmmoLabel.text = str(ammo) + " / " + str(tricet)
 # Tahle funkce provede strileni
 func _shoot():
 	var projectile := projectile_scene.instantiate()

fe3c629dc2dc27cfe0c1679dbe5c6f1044ddda0a
Macarat2009 <mat.becvar@gmail.com>
2025-05-19 16:09:10 +0200

    vsechny srdce

--- godot_project/sceny/hra.gd
+++ godot_project/sceny/hra.gd
@@ -5,9 +5,14 @@ extends Node2D
 func _ready() -> void:
 	%hrac.smrt.connect(_schovej_srdce.bind())
 
-func _schovej_srdce():
-	
-	$CanvasLayer/SrdceFialove.visible = false
+func _schovej_srdce(zivoty):
+	match zivoty:
+		2:
+			$CanvasLayer/SrdceFialove.visible = false
+		1:
+			$CanvasLayer/SrdceFialove2.visible = false
+		0:
+			$CanvasLayer/SrdceFialove3.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
